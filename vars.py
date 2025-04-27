23d1141d010f52284e1c063a8d8dbe876c770111
Gamerbot1212 <gbot03558@gmail.com>
2025-04-27 23:08:09 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "20831039")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "ea20b722f7af827db12fb85f4d55238c") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8023915313:AAFJ0x-h5TBZi8y457i-evSaA7cKk3-TYeA") #Replace with your bot token
